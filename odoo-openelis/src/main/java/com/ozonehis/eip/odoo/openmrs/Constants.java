f81055df58d1fc43de1cd35c1acde06d4a3873ad
Siddharth Vaish <sidvaish97@gmail.com>
2025-11-28 17:06:55 +0530

    Initial commit

---
+++ odoo-openelis/src/main/java/com/ozonehis/eip/odoo/openmrs/Constants.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright © 2025, Ozone HIS <info@ozone-his.com>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.ozonehis.eip.odoo.openmrs;
+
+public class Constants {
+
+    public static final String PARTNER_MODEL = "res.partner";
+
+    public static final String SALE_ORDER_MODEL = "sale.order";
+
+    public static final String SALE_ORDER_LINE_MODEL = "sale.order.line";
+
+    public static final String COUNTRY_STATE_MODEL = "res.country.state";
+
+    public static final String COUNTRY_MODEL = "res.country";
+
+    public static final String UOM_MODEL = "uom.uom";
+
+    public static final String PRODUCT_MODEL = "product.product";
+
+    public static final String IR_MODEL = "ir.model.data";
+
+    public static final String CREATE_METHOD = "create";
+
+    public static final String SEARCH_READ_METHOD = "search_read";
+
+    public static final String SEARCH_METHOD = "search";
+
+    public static final String WRITE_METHOD = "write";
+
+    public static final String UNLINK_METHOD = "unlink";
+
+    public static final String HEADER_ODOO_ID_ATTRIBUTE_VALUE = "odoo.attribute.value";
+
+    public static final String EXCHANGE_PROPERTY_SKIP_ENCOUNTER = "skip.encounter";
+
+    public static final String FHIR_OPENMRS_SYSTEM_PREFIX = "http://fhir.openmrs.org/";
+
+    public static final String FHIR_OPENMRS_EXT_PREFIX = FHIR_OPENMRS_SYSTEM_PREFIX + "ext/";
+
+    public static final String FHIR_OPENMRS_FHIR_EXT_MEDICINE = FHIR_OPENMRS_EXT_PREFIX + "medicine";
+
+    public static final String FHIR_OPENMRS_EXT_DRUG_NAME = FHIR_OPENMRS_FHIR_EXT_MEDICINE + "#drugName";
+}
---
+++ odoo-openelis/src/main/java/com/ozonehis/eip/odoo/openmrs/config/EIPAppConfig.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright © 2025, Ozone HIS <info@ozone-his.com>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.ozonehis.eip.odoo.openmrs.config;
+
+import ca.uhn.fhir.rest.client.api.IGenericClient;
+import com.ozonehis.eip.odoo.openmrs.ProductSynchronizer;
+import com.ozonehis.eip.odoo.openmrs.client.OdooFhirClient;
+import com.ozonehis.eip.odoo.openmrs.client.OpenmrsRestClient;
+import org.openmrs.eip.app.config.AppConfig;
+import org.openmrs.eip.fhir.spring.OpenmrsFhirAppConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * Import the {@link AppConfig} class to ensure that the required beans are created.
+ */
+@Configuration
+@Import({AppConfig.class, OpenmrsFhirAppConfig.class})
+@EnableScheduling
+public class EIPAppConfig {
+
+    @Bean
+    public ProductSynchronizer productCatalogSynchronizer(
+            OdooFhirClient odooFhirClient, IGenericClient openmrsFhirClient, OpenmrsRestClient openmrsRestClient) {
+        return new ProductSynchronizer(odooFhirClient, openmrsFhirClient, openmrsRestClient);
+    }
+}
