52aa6ae739b0d713a3c7de50ab8e19a9efea6e57
wluyima <wyclif@openmrs.org>
2026-01-06 20:52:02 -0600

    Updated timestamp store to support multiple resource

--- src/main/java/org/ozonehis/eip/odoo/openelis/task/TimeStampStore.java
+++ src/main/java/org/ozonehis/eip/odoo/openelis/task/TimeStampStore.java
@@ -3,15 +3,16 @@ package org.ozonehis.eip.odoo.openelis.task;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.hl7.fhir.r4.model.Resource;
 import org.ozonehis.eip.odoo.openelis.Constants;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
+import java.util.Properties;
 
 import static java.time.ZoneId.systemDefault;
 import static java.time.format.DateTimeFormatter.ISO_OFFSET_DATE_TIME;
@@ -23,55 +24,63 @@ public class TimeStampStore {
     @Value("${" + Constants.PROP_TASK_LAST_RUN_TS_FILE + "}")
     private String filename;
 
-    private File file;
-
     private LocalDateTime timestamp;
 
-    public LocalDateTime getLastRunDatetime() {
-        if (file == null) {
-            file = getFile();
-        }
+    private Properties props;
 
-        try {
-            log.info("Loading saved timestamp");
-            String datetime = FileUtils.readFileToString(file, StandardCharsets.UTF_8);
-
-            if (StringUtils.isBlank(datetime)) {
-                log.info("No saved timestamp found");
-            } else {
-                timestamp = ZonedDateTime.parse(datetime, ISO_OFFSET_DATE_TIME).withZoneSameInstant(systemDefault()).toLocalDateTime();
-                log.info("Done loading saved timestamp");
-            }
-        } catch (IOException e) {
-            throw new RuntimeException("Failed to read timestamp file", e);
-        }
-
-        return timestamp;
-    }
-
-    public void save(LocalDateTime timestamp) {
-        File f = getFile();
-        if (log.isDebugEnabled()) {
-            log.debug("Saving timestamp");
-        }
-
-        try {
-            FileUtils.writeStringToFile(f, timestamp.toString(), StandardCharsets.UTF_8);
+    public LocalDateTime getTimestamp(Class<Resource> resourceType) {
+        final String ts = getProps().getProperty(resourceType.getSimpleName());
+        if (StringUtils.isBlank(ts)) {
             if (log.isDebugEnabled()) {
-                log.debug("Successfully saved timestamp");
+                log.debug("No saved timestamp found for {} resource", resourceType.getSimpleName());
             }
-        } catch (IOException e) {
-            throw new RuntimeException("Failed to save timestamp", e);
+
+            return null;
         }
 
-        this.timestamp = timestamp;
+        return ZonedDateTime.parse(ts, ISO_OFFSET_DATE_TIME).withZoneSameInstant(systemDefault()).toLocalDateTime();
     }
 
-    private File getFile() {
-        if (file == null) {
-            file = new File(filename);
+    public void update(LocalDateTime timestamp, Class<Resource> resourceType) {
+        final String resource = resourceType.getSimpleName();
+        Properties propsTemp = new Properties(getProps());
+        final String newTimestamp = timestamp.format(ISO_OFFSET_DATE_TIME);
+        propsTemp.put(resourceType.getSimpleName(), newTimestamp);
+        if (log.isDebugEnabled()) {
+            log.debug("Updating timestamps for {} resource to {}", resource, newTimestamp);
         }
 
+        try {
+            propsTemp.store(FileUtils.openOutputStream(getFile()), null);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to save timestamps for " + resource + " resource ", e);
+        }
+
+        getProps().put(resourceType.getSimpleName(), timestamp.format(ISO_OFFSET_DATE_TIME));
+        if (log.isDebugEnabled()) {
+            log.debug("Successfully saved timestamps for {} resource", resource);
+        }
+    }
+
+    private Properties getProps() {
+        if (props == null) {
+            props = new Properties();
+
+            try {
+                File file = getFile();
+                log.info("Loading timestamps from {}", file);
+                props.load(FileUtils.openInputStream(file));
+                log.info("Successfully loaded timestamps");
+            } catch (IOException e) {
+                throw new RuntimeException("Failed to load timestamps", e);
+            }
+        }
+
+        return props;
+    }
+
+    protected File getFile() {
+        File file = new File(filename);
         if (!file.exists()) {
             if (file.getParentFile().exists()) {
                 log.info("Creating timestamp directory");
