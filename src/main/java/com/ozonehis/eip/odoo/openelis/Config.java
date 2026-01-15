4fd022b86916ae15652b26a97f62b6576bf556bb
wluyima <wyclif@openmrs.org>
2026-01-14 20:59:37 -0600

    Updated spring configuration and packaging of the final jar

--- src/main/java/com/ozonehis/eip/odoo/openelis/Config.java
+++ src/main/java/com/ozonehis/eip/odoo/openelis/Config.java
@@ -3,18 +3,13 @@ package com.ozonehis.eip.odoo.openelis;
 import com.ozonehis.eip.odoo.openelis.fhir.OdooFhirClient;
 import com.ozonehis.eip.odoo.openelis.fhir.OpenElisFhirClient;
 import com.ozonehis.eip.odoo.openelis.task.TaskConfig;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+@Configuration
 @Import(TaskConfig.class)
-public class Launcher {
-
-    public static void main(String[] args) {
-        SpringApplication.run(Launcher.class, args);
-    }
+public class Config {
 
     @Bean
     public StartListener startListener(OpenElisFhirClient openElisClient, OdooFhirClient odooClient) {
