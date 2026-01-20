4dcc0afb953b7c26eacc4896747990763d672447
wluyima <wyclif@openmrs.org>
2026-01-20 17:42:41 -0600

    Moved FhirController to web sub package

--- src/main/java/com/ozonehis/eip/odoo/openelis/fhir/web/FhirController.java
+++ src/main/java/com/ozonehis/eip/odoo/openelis/fhir/web/FhirController.java
@@ -1,6 +1,7 @@
-package com.ozonehis.eip.odoo.openelis.fhir;
+package com.ozonehis.eip.odoo.openelis.fhir.web;
 
 
+import com.ozonehis.eip.odoo.openelis.fhir.OdooFhirClient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
